33701ec8612f18de224fee23fb0cc9baff7bafd2
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-06 11:05:59 +0000

    basic daily budget analysis

---
+++ daily commits/accounts/daily_expense.py
@@ -0,0 +1,28 @@
+#intially we are summing up the expenses
+
+x=int(input("Enter your Today's Expense : "))
+y=int(input("Enter your Today's income Here: "))
+
+net=y-x
+
+#print(net)
+#setting balance
+balance=0
+balance=balance+net
+
+#calculating percentage
+ratio=x/y
+perc=ratio*100
+#showing balance
+print("your current bank balance is Rs.", balance)
+
+#basic final report with if else
+if (ratio>=y*0.6):
+  print("congrats,your today's expense is only",perc,"percentage of your income,keep saving")
+elif(ratio>=0.3 and ratio<0.6):
+  print("Good,your today's expense is only",perc,"percentage of your income,keep maintaining your expenses")
+elif(ratio>=0 and ratio<0.3):
+  print("Alert!!!...you spent almost",perc,"percentage of the income today.think before spending")
+else:
+  print("Alert....your today's expense is morethan income.it will be risky in future")
+  #end 
