5ce81ddb27cc829f570d32ad91405b4411938fbc
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-07 18:00:22 +0000

    new test

--- daily commits/accounts/daily_expense.py
+++ daily commits/accounts/daily_expense.py
@@ -31,13 +31,15 @@ perc=ratio*100
 #showing balance
 print("your current bank balance is Rs.", balance)
 
+
 #basic final report with if else
-if (ratio>=y*0.6):
-  print("congrats,your today's expense is only",perc,"percentage of your income,keep saving,we will introduce you better financial plannings soon")
-elif(ratio>=0.3 and ratio<0.6):
-  print("Good,your today's expense is only",perc,"percentage of your income,keep maintaining your expenses")
-elif(ratio>=0 and ratio<0.3):
-  print("Alert!!!...you spent almost",perc,"percentage of the income today.think before spending")
-else:
-  print("Alert....your today's expense is morethan income.you have better to reduce the expenses or increase the income daily")
+if serv==3:
+  if (ratio>=y*0.6):
+   print("congrats,your today's expense is only",perc,"percentage of your income,keep saving,we will introduce you better financial plannings soon")
+  elif(ratio>=0.3 and ratio<0.6):
+   print("Good,your today's expense is only",perc,"percentage of your income,keep maintaining your expenses")
+  elif(ratio>=0 and ratio<0.3):
+   print("Alert!!!...you spent almost",perc,"percentage of the income today.think before spending")
+  else:
+   print("Alert....your today's expense is morethan income.you have better to reduce the expenses or increase the income daily")
   #end 
