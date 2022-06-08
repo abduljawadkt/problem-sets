d7a71c88f0b2bc28f3a563b3cd571d045f5cef83
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-08 18:07:34 +0000

    zero division error

--- daily commits/accounts/daily_expense.py
+++ daily commits/accounts/daily_expense.py
@@ -3,6 +3,10 @@
 service=['1.Individual input','2.Daily input','3.Report']
 print("choose the service from the following")
 print(service)
+
+net_income=0
+net_expense=0
+balance=0
 #choosing service and redirecting to it
 serv=int(input("Enter Here: "))
 while (serv>3 or serv<1):
@@ -13,24 +17,26 @@ while (serv>3 or serv<1):
 if serv==1:
   out1=0
   serv1=int(input("Enter Income/Expense here(use negative sign if it is expense): "))
-  out1=out1+serv1
-  
-  
+  balance=balance+serv1
+  if serv1>0:
+    net_income=net_income+serv1
+    balance=balance+net_income
+  else:
+    net_expense=net_expense+serv1
+    balance=balance+net_expense
   
 #daily input
 elif serv==2:
   net=0
   x=int(input("Enter your Today's Expense : "))
   y=int(input("Enter your Today's income Here: "))
-  net=y-x
+  net_expense=net_expense+x
+  net_income=net_income+y
+  balance=balance+net
 
-#print(net)
-#setting balance
-balance=0
-balance=balance+net+out1
 
 #calculating percentage
-ratio=x/y
+ratio=net_expense/net_income  
 perc=ratio*100
 #showing balance
 print("your current bank balance is Rs.", balance)
