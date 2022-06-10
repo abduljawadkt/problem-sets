fa77fa5700b32beb91831d8debf0b465ab68fb28
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-10 16:37:25 +0000

    init

---
+++ daily commits/accounts/daily_expense(2).py
@@ -0,0 +1,9 @@
+#function defining initially
+def __init__(self):
+        self.income = 0
+        self.expenses = 0
+        self.expense_list = []
+        self.expense_name = []
+        self.income_name = []
+        self.income_list = []
+        
