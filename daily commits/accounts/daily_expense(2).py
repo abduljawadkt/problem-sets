2c1dfd5356dbb4d3319f4034bfda8bdce167cc1d
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-15 17:44:50 +0000

    functions part

--- daily commits/accounts/daily_expense(2).py
+++ daily commits/accounts/daily_expense(2).py
@@ -7,9 +7,7 @@ def __init__(self):
         self.income_name = []
         self.income_list = []
  #income adding       
-def income_ask(self):
-        add_income = input('Add income? [y/n]: ')
-        return add_income
+
 def income_sum(self):
         self.income = sum(self.income_list)
 def expense_ask(self):
