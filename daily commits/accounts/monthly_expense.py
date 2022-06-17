35b4b3686a2da752d27dc9b8b649e34a09d113a5
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-17 04:39:48 +0000

    income

---
+++ daily commits/accounts/monthly_expense.py
@@ -0,0 +1,11 @@
+#building a monthly expense calculator
+
+#Greeting
+print("Welcome to budget estimator!!!")
+
+#total income of the month
+income=float(input("Enter your monthly income: "))
+additionals=float(input("Enter your additional income in this month: "))
+
+total_income=income+additionals
+
