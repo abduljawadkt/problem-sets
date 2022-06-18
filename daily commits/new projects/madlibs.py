969fef6fc38f892950d2752b99b5bb0983bd2f0e
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-18 08:42:11 +0000

    madlib

--- daily commits/new projects/madlibs.py
+++ daily commits/new projects/madlibs.py
@@ -1,9 +1,20 @@
 #string concatenation
 #suppose we want to create a string that says 'subscribe to _____'
 #3 ways we checking
-youtuber=""#some string variable
+#youtuber="tech travel eat"
 
-print("subscribe to " +youtuber)
-print("subsctibe to {}".format(youtuber))
+#print("subscribe to" + youtuber)
+#print("subsctibe to {}".format(youtuber))
 #usage of fstring
-print(f"subscribe to {youtuber}")
+#print(f"subscribe to {youtuber}")
+
+
+#lets move into madlib now
+#job analysis
+job=input("Enter specified job: ")
+feel0=input("your opinion about that job: ")
+feel1=input("How you feels about it: ")
+person=input("who is that person famous in this field: ")
+feel2=input("How you feel about him: ")
+madlib = f"{job}ing is a verymuch {feel0} process.i {feel1} to that job.sometimes i feel {feel2} about {person} that he is doing {job}ing."
+print(madlib)
