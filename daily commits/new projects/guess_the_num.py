23bbba40d980af7d8ebe196695a9c512c8dcbada
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-19 00:45:19 +0000

    guess the number

--- daily commits/new projects/guess_the_num.py
+++ daily commits/new projects/guess_the_num.py
@@ -17,3 +17,4 @@ def guess(x):
       print("sorry,Guess again.Too low")
     elif guess > random_number:
       print("sorry,guess again.too high")
+  print(f"congrats,you picked the correct number{random_number}correctly")         
