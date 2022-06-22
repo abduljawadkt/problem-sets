e074abb4f6a63794c5a9bf7cbb0eebb2d87da67b
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-22 16:09:59 +0000

    play

--- daily commits/new projects/rock_paper_scissor.py
+++ daily commits/new projects/rock_paper_scissor.py
@@ -1,13 +1,17 @@
 import random
 
 def play():
-  user=input("what is your choice?'r' for rock,'p' for paper,'s' for scissor")
+  user=input("what is your choice?'r' for rock,'p' for paper,'s' for scissor: ")
   computer=random.choice(['r','p','s'])
   if user==computer:
     return 'tie'
+  if is_win(user,computer):
+    return 'You Won!'
+  
+  return "You lost"
+    
 
 #condition for mismatch
-
 def is_win(player,opponent):
   if (player=='r' and opponent=='s') or (player=='s' and opponent=='p') or  (player=='p' and opponent=='r'):
     return True
