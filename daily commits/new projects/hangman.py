bda6bf6847dcb8883a7ac4bda14d5bcb89174d05
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-23 05:37:54 +0000

    game structure

--- daily commits/new projects/hangman.py
+++ daily commits/new projects/hangman.py
@@ -9,3 +9,18 @@ def get_valid_word(words):
   while '' in word or '-' in word:
     word=random.choice(words)
   return words  
+
+#hangman
+def hangman():
+    word=get_valid_word(words)
+    word_letter=set(word) #letters of the word
+    alphabet=set(string.ascii_uppercase) #letter of the english dictionary
+    used_letters=set()  #what the letters user has guessed
+    
+    #user input
+    user_letter=input("Enter a letter: ")
+    if user_letter in alphabet-used_letters:
+        used_letters.add(user_letter)
+        is user_letter in word_letter:
+            word_letter.remove(user_letter)
+  
