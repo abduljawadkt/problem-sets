438b0cb4c313ccf4208ded6acc14476327bb76bb
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-25 16:02:43 +0000

    player

--- daily commits/new projects/tic_tac_toe/player.py
+++ daily commits/new projects/tic_tac_toe/player.py
@@ -7,7 +7,25 @@ class player:
   def __init__(self,letter):
     #it is x or o
     self.letter=letter
+  #we want to define the move of a player
+  def get_move(self,game):
+    pass
+#placeholder.
+
+class computerplayer(player):
+  #method of inheritance is used by taking class player as a parent class
+  def __init__(self,letter):
+    super().__init__(letter)
+    #superclass is used here.#temporary
 
   def get_move(self,game):
     pass
+    
+class humanplayer(player):
+  #inheritance
+  def __init__(self,letter):
+    super().__init__(letter)
 
+  def get_move(self,game):
+    pass
+    
