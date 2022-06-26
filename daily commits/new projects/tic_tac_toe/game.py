dfe4a740bfc304e3d05a11190f7e232cacec121f
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-26 13:07:29 +0000

    boards

--- daily commits/new projects/tic_tac_toe/game.py
+++ daily commits/new projects/tic_tac_toe/game.py
@@ -1,6 +1,8 @@
 class tictactoe:
   def __init__(self):
-    self.board=['' for _ in range(9)]#using single list to represent a 3X3 board
+    self.board=['' for _ in range(9)]#using single list to represent a 3X3 board, we get ['','','',....upto 9]
+    
+    
     self.current_winner=None #keep track of the winner,initialize with no winner
 
   def print_board(self):
@@ -8,4 +10,16 @@ class tictactoe:
     for row in [self.board[i*3:(i+1)*3)] for i in range(3)]:
       print('|'+'|'.join(row)+'|')
       #printing the rows
-  
+      #||||
+      #||||
+      #||||,output
+  def print_board_nums():
+    number_board=str(i) for i in range(j*3,(j+1)*3) for j in range(3)
+#out |1|2|3|
+    for row in number_board:
+      print('|'+'|'.join(row)+'|')
+     #|0|1|2|
+     #|3|4|5|
+     #|6|7|8| 
+      
+    
