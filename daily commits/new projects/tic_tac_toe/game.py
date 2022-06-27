aec9c39b2196e3354543d1d2055b152a35087f5c
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-27 10:27:03 +0000

    check result

--- daily commits/new projects/tic_tac_toe/game.py
+++ daily commits/new projects/tic_tac_toe/game.py
@@ -35,12 +35,57 @@ class tictactoe:
   def num_empty_squares(self):
     return self.board.count('')
     #return number of empty squares
+
+  def make_move(self,square,letter):
+    #if valid move,it will assign square to letter
+    if self.board[square]=='':
+      self.board[square]=letter
+      if self.winner(square,letter):
+        self.current_winner=letter
+        
+      return True
+    return False  
+
+  def winner(self,square,letter):
+    #for that we need to check the row
+    row_ind=square//3
+    row=self.board[row_ind*3:(row_ind+1)*3]
+    if all([spot==letter for spot in  row]):
+      return True
+    #check_coloumn
+    col_ind=square % 3
+    column=[self.board[col_ind+i*3] for i in range(3)]
+    if all(spot==letter for spot in column]):
+      return True
+    #check_diagonals  
     
+
 
 def play(game,x_player,o_player,print_game=True):
   if print_game:
     game.print_board_nums()
       #return number board with index nums.(above given)
+  letter = 'X'
+  while game.empty_squares():
+    if letter=='O':
+      square=o_player.get_move(game)
+    else:
+      square=x_player.get_move(game)
       
-      
-    
+    if game.make_move(square,letter):
+      if print_game():
+        print(letter + f'make move to {square}')
+        game.print_board()
+        print('')
+        #just empty line  
+
+      if game.current_winner:
+        if print_game:
+          print(letter+'wins!')
+        return letter  
+          
+      letter='O' if letter =='X' else 'X'
+        #this is equalent to if else statement
+
+    if print_game:
+      print('it\s a tie!')
--- daily commits/new projects/tic_tac_toe/player.py
+++ daily commits/new projects/tic_tac_toe/player.py
@@ -27,15 +27,20 @@ class humanplayer(player):
     super().__init__(letter)
 
   def get_move(self,game):
-    valid_square=false
+    valid_square=False
     val=None
     while not valid_square:
       square=input(self.letter+'\'s turn.input move(0-9):')
       try:
         val=int(square)
         if val not in game.available_moves():
+          #chech whether the move could be done
           raise ValueError
+          
         valid square=True
+        #if the above code become successful
+        #try-except
+        #error handling
       except ValueError:
         print('Invalid Square,Try again')
     return val    
