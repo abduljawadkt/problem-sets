14aa5f3f8124f37939d29caadc3e59038f3378bd
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-06-30 23:06:04 +0530

    game final

--- daily commits/new projects/tic_tac_toe/game.py
+++ daily commits/new projects/tic_tac_toe/game.py
@@ -1,3 +1,5 @@
+from player import humanplayer ,computerpalyer
+
 class tictactoe:
   def __init__(self):
     self.board=['' for _ in range(9)]#using single list to represent a 3X3 board, we get ['','','',....upto 9]
@@ -53,14 +55,35 @@ class tictactoe:
     row_ind=square//3
     #we get row index while doing stage division
     row=self.board[row_ind*3:(row_ind+1)*3]
+    #the above formula return the index of row
+    #roundoff method is using here
+
+
+    
     if all([spot==letter for spot in  row]):
+      #if all means that if everything in the list is true
       return True
-    #check_coloumn
+    
+    #we need to check_coloumn also
     col_ind=square % 3
+    #reminder method is using here
+    #above formula returns the column number
     column=[self.board[col_ind+i*3] for i in range(3)]
     if all(spot==letter for spot in column]):
       return True
-    #check_diagonals  
+    
+    #next we want to check_diagonals
+      #digonals only have index numbers 0,2,4,6,8
+      #lets make a formula for it
+    if square%2==0:
+       diagonal1=[self.board[i] for i in [0,4,8]]
+       if all(spot==letter for spot in diagonal1):
+         return True
+       diagonal2=[self.board[i] for i in [2,4,6]]
+       if all(spot==letter for spot in diagonal2):
+         return True
+    #if all these checks fails
+    return False     
     
 
 
@@ -92,3 +115,10 @@ def play(game,x_player,o_player,print_game=True):
 
     if print_game:
       print('it\s a tie!')
+
+if __name__=='__main__':
+  x_player=humanplayer('X')
+  o_player=computerplayer('O')
+  t=tictactoe()
+  play(t,x_player,o_player,print_game=True)
+  
