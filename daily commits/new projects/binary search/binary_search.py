c676c06c711e335690f80c5158781e8a5dba05bb
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-07-04 06:26:18 +0000

    intro-naive search

---
+++ daily commits/new projects/binary search/binary_search.py
@@ -0,0 +1,14 @@
+#we need to write code for binary search here
+#we have to show that binary search is faster than naive search
+#algorithm for naive search
+#algorithm for binary search
+
+def naive_search(l,target):
+  #l is the list and taget is the element to be find
+  for i in range(len(l)):
+    if l[i]==target:
+      return i
+  return -1
+#basic code for naive search
+#l=[1,2,3,4,5,6,7]
+#naive_search(l,5)
