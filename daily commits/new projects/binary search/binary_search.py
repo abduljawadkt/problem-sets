56f47f607c19ea7a7bce5b0e38de6ad1a57117e5
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-07-04 06:47:26 +0000

    code structure

--- daily commits/new projects/binary search/binary_search.py
+++ daily commits/new projects/binary search/binary_search.py
@@ -12,3 +12,18 @@ def naive_search(l,target):
 #basic code for naive search
 #l=[1,2,3,4,5,6,7]
 #naive_search(l,5)
+
+
+#lets move to binary search
+#binary search is a midpoint based searching mechanism
+def binary_search(l,target):
+  midpoint=len(l)//2
+
+  #if midpoint,we return midpoin itself
+  if l[midpoint]==target:
+    return midpoint
+
+  elif target<l[midpoint]:
+    return binary_search(l,target)
+  else:
+    return binary_search(l,target)
