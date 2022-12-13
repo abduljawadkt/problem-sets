bb8ec81a3d4463645758fe7d912faa6ca6fcec3f
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-12-13 18:14:06 +0000

    basic structure

--- daily commits/minesweaper.py
+++ daily commits/minesweaper.py
@@ -4,5 +4,15 @@ class Board:
   __init__(self, dim_size, num_bombs):
   self.dim_size = dim_size
   self.num_bombs = num_bombs
+  #initialised a board class and added attributes towards it
+
+  self.board = self.make_new_board()
+  #to plant new bombs
+
+  self.dug = set() 
+
+  def make_new_board(self)
+
+
+  
 def play (dim_size == 10 , num_bombs == 10 )
-hjka
