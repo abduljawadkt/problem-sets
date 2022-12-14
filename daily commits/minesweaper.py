424d02a2f86040f51992fad8d54970f6c4d9a019
ABDUL JAWAD <70905521+abduljawadkt@users.noreply.github.com>
2022-12-14 17:17:41 +0000

    added board

--- daily commits/minesweaper.py
+++ daily commits/minesweaper.py
@@ -9,10 +9,16 @@ class Board:
   self.board = self.make_new_board()
   #to plant new bombs
 
-  self.dug = set() 
+  self.dug = set() #if we dig at 0, 0,then self.dug = {(0,0)}
 
   def make_new_board(self)
+  #construct a new board based on dimension size and num bombs
+  #we should construct list of lists
+  #but it is a 2D board
 
+  #generating a new board
+  Board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
 
   
-def play (dim_size == 10 , num_bombs == 10 )
+def play (dim_size == 10 , num_bombs == 10 ):
+  #step 1 : create a new board and plant new bombs
